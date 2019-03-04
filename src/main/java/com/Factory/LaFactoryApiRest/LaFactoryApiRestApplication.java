b53a170b0c7706b910dcd4adf1c8291ece9a4075
neorey <jeroenvdsteen@hotmail.fr>
2019-03-04 12:31:02 +0100

    initial commit

---
+++ src/main/java/com/Factory/LaFactoryApiRest/LaFactoryApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.Factory.LaFactoryApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFactoryApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaFactoryApiRestApplication.class, args);
+	}
+
+}
