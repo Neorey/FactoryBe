5bcd2bdce18684a51872311710f7879e147b9f55
HamzaTrq1 <hamza.tariq095@gmail.com>
2019-03-05 10:40:31 +0100

    ajout userRepository

--- src/main/java/com/Factory/entity/Formation.java
+++ src/main/java/com/Factory/entity/Formation.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -22,7 +23,7 @@ public class Formation {
 	private int version;
 	@Id
 	@Column(name="matiere_id")
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 
 	public Formation() {
